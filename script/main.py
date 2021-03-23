0e4992d32f7ef8d6998e520dd924ea68de56fda6
fahim.hadi <fahimhadimaula@gmail.com>
2021-03-23 17:41:50 +0700

    set webhook

--- script/main.py
+++ script/main.py
@@ -3,8 +3,46 @@ import telegram as tl
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
 import logging
 import re
-import os
 from pymongo import MongoClient
+# from flask import Flask, request
+import threading
+
+# setup bot
+bot = Updater(keys.telegram_key, use_context=True)
+
+# setup flask
+# app = Flask(__name__)
+
+# setup db
+cluster = MongoClient(keys.mongodb_key)
+db = cluster['compass_chatbot']['user_profile']
+
+# setup data
+global temp_profile
+temp_profile = {}
+global temp_dest
+temp_dest = {}
+
+first_name = []
+for i in db.find({}):
+    first_name.append(i['first_name'])
+
+def update_data(save=False, **kwargs):
+    temp_profile['_id'] = kwargs['id']
+    temp_profile['first_name'] = kwargs['firstname']
+    temp_profile['username'] = kwargs['username']
+    temp_profile['domisili'] = kwargs['dom']
+    temp_profile['gender'] = kwargs['gen']
+    temp_profile['age'] = kwargs['age']
+    if save == True:
+        return db.insert_one(temp_profile), temp_profile
+    else:
+        return temp_profile
+
+def update_dest(save=False, **kwargs):
+    temp_dest['destination'] = kwargs['d']
+    temp_dest['time'] = kwargs['t']
+    return temp_dest
 
 # States, as integers
 WELCOME1 = 0
@@ -19,16 +57,9 @@ TIME = 8
 TRAVELPLAN = 9
 CANCEL = 100
 
-# setuo db
-cluster = MongoClient(keys.mongodb_key)
-db = cluster['compass_chatbot']['user_profile']
-
-temp_profile = {}
-temp_dest = {}
-
-first_name = []
-for i in db.find({}):
-    first_name.append(i['first_name'])
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # /start
 def start(update, context):
@@ -44,37 +75,6 @@ def start(update, context):
                                     reply_markup=tl.ReplyKeyboardMarkup([['Mau tau tempat oke dong!', 'Bikinin itinerary bisa kali!']], one_time_keyboard=True))
         return WELCOME2
 
-def update_data(kw, save=False, **kwargs):
-    global temp_profile
-    if kw == 'id':
-        temp_profile['_id'] = kwargs['id']
-        temp_profile['first_name'] = kwargs['firstname']
-        temp_profile['username'] = kwargs['username']
-
-    if kw == 'dom':
-        temp_profile['domisili'] = kwargs['dom']
-    
-    if kw == 'gen':
-        temp_profile['gender'] = kwargs['gen']
-
-    if kw == 'age':
-        temp_profile['age'] = kwargs['age']
-
-    if save == True:
-        return db.insert_one(temp_profile), temp_profile
-    else:
-        return temp_profile
-
-def update_dest(kw, save=False, **kwargs):
-    global temp_dest
-    if kw == 'd':
-        temp_dest['destination'] = kwargs['d']
-
-    if kw == 't':
-        temp_dest['time'] = kwargs['t']
-    
-    return temp_dest
-
 def welcome1(update, context):
     if update.message.text.lower() in ['yes', 'ya', 'y']:
         update_data(kw='id', id=update.message.from_user['id'], firstname=update.message.from_user['first_name'], username=update.message.from_user['username'])
@@ -84,13 +84,13 @@ def welcome1(update, context):
         return CANCEL
 
 def profile_a(update, context):
-    update_data(kw='dom', dom=update.message.text)
+    update_data(dom=update.message.text)
     update.message.reply_text('Gender anda?',
                                 reply_markup=tl.ReplyKeyboardMarkup([['Pria', 'Wanita']], one_time_keyboard=True))
     return PROFILE_B
 
 def profile_b(update, context):
-    update_data(kw='gen', gen=update.message.text)
+    update_data(gen=update.message.text)
     update.message.reply_text('Berapakah umur anda saat ini?')
     return PROFILE_C
 
@@ -115,7 +115,6 @@ def welcome2(update, context):
                     [tl.InlineKeyboardButton('Jogjakarta', url='https://travelspromo.com/?s=jogja', callback_data=4)],
                     [tl.InlineKeyboardButton('Mandalika', url='https://travelspromo.com/?s=mandalika', callback_data=5)],
                     [tl.InlineKeyboardButton('Labuan Bajo', url='https://travelspromo.com/?s=labuan+bajo', callback_data=6)]]
-
         reply_markup = tl.InlineKeyboardMarkup(keyboard)
         update.message.reply_text('Ini nih tempat-tempat keren dan populer yang bisa kalian pilih saat ini:', reply_markup = reply_markup)
         return CANCEL
@@ -135,20 +134,16 @@ def definetime(update, context):
     update.message.reply_text(f"{waktu} waktu yang tepat sih.")
     return TRAVELPLAN
 
-# def travelplan(update, context):
-
-
 def cancel(update, context):
     update.message.reply_text(f"Anda memilih {update.message.text}")
     return ConversationHandler.END
 
+# @app.route('/{}'.format(keys.telegram_key), methods=['POST'])
 def main():
-    updater = Updater(keys.telegram_key, use_context=True)
-    dp = updater.dispatcher
+    dp = bot.dispatcher
     yes_no_regex = re.compile(r'^(yes|no|ya|y|n|tidak|tdk)$', re.IGNORECASE)
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-    handler = tl.ext.ConversationHandler(
+    handler = ConversationHandler(
                 entry_points=[CommandHandler('start', start)],
                 states={
                     WELCOME1: [MessageHandler(Filters.regex(yes_no_regex), welcome1)],
@@ -163,12 +158,21 @@ def main():
                 fallbacks=[CommandHandler('cancel', cancel)]
             )
     dp.add_handler(handler)
-
     if keys.ENV == 'DEV':
-        updater.start_polling()
-        updater.idle()
+        bot.start_polling()
     elif keys.ENV == 'PROD':
-        updater.start_webhook(listen=keys.host, port=keys.port, url_path=keys.telegram_key, webhook_url='https://compasschatbot.herokuapp.com/' + keys.telegram_key)
-        updater.idle()
+        bot.start_webhook(listen='0.0.0.0', port=keys.port, url_path=keys.telegram_key)
+        bot.bot.set_webhook('https://compasschatbot.herokuapp.com/' + keys.telegram_key)
 
-main()
+    # if keys.ENV == 'DEV':
+    #     updater.start_polling()
+    #     # updater.idle()
+    # elif keys.ENV == 'PROD':
+    #     updater.start_webhook(listen=keys.host, port=keys.port, url_path=keys.telegram_key, webhook_url='https://compasschatbot.herokuapp.com/' + keys.telegram_key)
+    #     updater.idle()
+
+# main()
+
+# run
+if __name__ == '__main__':
+    main()
